fa508f81959a38934099fdd06699ca403205bbc2
Arnaud Bravard <bravard.arnaud@gmail.com>
2023-06-06 16:50:25 +0200

    modification de contenu

--- book-detail.php
+++ book-detail.php
@@ -5,14 +5,17 @@ $bookDetails = getDetailBook();
 ?>
 
 
-<h2 class="text-center">Book TITLE</h2>
+<h2 class="text-center mb-5">Book TITLE</h2>
 
-<?php foreach ($bookDetails as $bookDetail) : ?>
-    <p> <?php echo $bookDetail['title'] ?> </p>
-    <p> <?php echo $bookDetail['book_id'] ?> </p>
-    <p> <?php echo $bookDetail['content'] ?> </p>
-<?php endforeach; ?>
-
+<div class="d-flex justify-content-center">
+    <?php foreach ($bookDetails as $bookDetail) : ?>
+        <div class="card p-5 ">
+            <p class="text-center"> <?php echo $bookDetail['title'] ?> </p>
+            <p class="text-center"> <?php echo $bookDetail['book_id'] ?> </p>
+            <p class="text-center"> <?php echo $bookDetail['content'] ?> </p>
+        </div>
+    <?php endforeach; ?>
+</div>
 
 <?php
 include("parts/footer.php");
--- functions.php
+++ functions.php
@@ -2,51 +2,51 @@
 
 
 // CONNEXION A LA BDD
-function connectDB(){
+function connectDB()
+{
     try {
         $host = 'localhost';
         $dbName = 'library';
         $user = 'root';
         $password = '';
-    
+
         return new PDO(
             'mysql:host=' . $host . ';dbname=' . $dbName . ';charset=utf8',
             $user,
             $password
         );
-    }
-    catch (Exception $e) {
-            
-        echo('Erreur connexion à la base de données : '.$e->getMessage());
-        
+    } catch (Exception $e) {
+
+        echo ('Erreur connexion à la base de données : ' . $e->getMessage());
+
         exit;
-       }
+    }
 }
 
 
-   // AFFICHER TOUS LES LIVRES DANS L'INDEX
-   function getAllBooks()
-   {
-    $pdo=connectDB();
+// AFFICHER TOUS LES LIVRES DANS L'INDEX
+function getAllBooks()
+{
+    $pdo = connectDB();
     $request = $pdo->prepare('SELECT * FROM books');
     $request->execute();
     return $request->fetchAll();
-   }
+}
 
 
-   // AFFICHER LE DETAIL D'UN LIVRE
-   function getDetailBook()
-   {
+// AFFICHER LE DETAIL D'UN LIVRE
+function getDetailBook()
+{
     $collect = (int) $_GET['id'];
     $pdo = connectDB();
     $request = $pdo->prepare("SELECT * FROM books WHERE book_id = $collect");
     $request->execute();
     return $request->fetchAll();
-   }
+}
 
-   // AJOUTER UN LIVRE
-   function addBook()
-   {
+// AJOUTER UN LIVRE
+function addBook()
+{
     $pdo = connectDB();
     $request = $pdo->prepare('INSERT INTO books (id_user, title, content) VALUES (:id_user, :title, :content)');
     $request->execute([
@@ -55,4 +55,4 @@ function connectDB(){
         "content" => $_POST['contentBook'],
     ]);
     return $request->fetchAll();
-   }
+}
